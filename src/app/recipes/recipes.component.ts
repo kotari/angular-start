df3abfbef5ecc0f197a9472ae4b7934b15ac0942
Me <me@alightsolutions.com>
2017-11-13 17:19:51 -0500

    still work needs to be done

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Recipe } from './recipe.model';
 
 @Component({
@@ -7,7 +7,7 @@ import { Recipe } from './recipe.model';
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
-  @Input() recipe: Recipe;
+  selectedRecipe: Recipe;
   constructor() { }
 
   ngOnInit() {
